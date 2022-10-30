be050ce6a613970af02f901bf36a6e388a361be9
Misael Borges <nexcsbr@gmail.com>
2022-10-30 10:08:16 -0300

    feat: add nav component to layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,10 @@
-export type LayoutProps = {
-  content: React.ReactNode;
-};
+import Nav from "./Nav";
+import { LayoutProps } from "@/types/layout.type";
 
 const Layout = ({ content }: LayoutProps) => {
   return (
     <>
+      <Nav />
       <main className="mx-auto max-w-full md:max-w-6xl min-h-screen px-5">
         {content}
       </main>
